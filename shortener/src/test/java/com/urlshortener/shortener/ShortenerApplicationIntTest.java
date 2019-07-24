9a6a5f5eff82686649477c37316315ca76a9229d
simranmalhi <46908296+simranmalhi@users.noreply.github.com>
2019-07-24 11:49:48 -0700

    working tests

--- shortener/src/test/java/com/urlshortener/shortener/ShortenerApplicationIntTest.java
+++ shortener/src/test/java/com/urlshortener/shortener/ShortenerApplicationIntTest.java
@@ -1,30 +1,4 @@
-
 package com.urlshortener.shortener;
-
-/*
-import com.urlshortener.shortener.json.LongUrl;
-import com.urlshortener.shortener.json.ShortCode;
-//import com.urlshortener.shortener.service.ShortUrlService;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
-
-//import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertTrue;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.skyscreamer.jsonassert.JSONAssert;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-*/
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -53,26 +27,9 @@ public class ShortenerApplicationIntTest {
     TestRestTemplate restTemplate;
     
     @Test
-    public void getUrl() throws Exception {
-        
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-        //headers.set("Accept", "application/json");
-        HttpEntity entity = new HttpEntity(headers);
-        ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/api/fetch?shortCode=ABBBBB"),
-                HttpMethod.GET,
-                entity,
-                String.class);        
-        
-        assertThat(response.getBody().isEmpty());
-    }
-    
-     @Test
     public void saveUrl() throws Exception {
-        
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "application/json");
-        //headers.set("Accept", "application/json");
 
         HttpEntity entity = new HttpEntity(headers);
 
@@ -81,102 +38,26 @@ public class ShortenerApplicationIntTest {
                 entity,
                 String.class);        
         
-            assertThat(response.getBody().contains("shortCode"));
-        }
+        assertThat(response.getBody().contains("shortCode"));
+    }
+    
+    @Test
+    public void getUrl() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+
+        HttpEntity entity = new HttpEntity(headers);
+        
+        ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/api/fetch?shortCode=ABBBBB"),
+                HttpMethod.GET,
+                entity,
+                String.class);        
+        
+        assertThat(response.getBody().isEmpty());
+    }
     
     private String createURLWithPort(String uri) {
         return "http://localhost:" + port + uri;
     }
 }
-
-
-/*
-@RunWith(SpringRunner.class)
-@WebMvcTest(ApiController.class) 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class ShortenerApplicationIntTest {
-      
-    String longURL = "http://abcd.com/test";        
-    String shortCode = "abcdfer";
-    
-    @LocalServerPort
-    private int port;
-     
-    @Autowired
-    TestRestTemplate restTemplate;
-    
-    HttpHeaders headers = new HttpHeaders();
-    
-    @Test
-    public void testCreateStudent() throws Exception {
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-        ResponseEntity<String> response = restTemplate.exchange(
-          createURLWithPort("/api/create?longUrl=" + longURL), HttpMethod.POST, entity, String.class);
-        String actual = response.getHeaders().get(HttpHeaders.LOCATION).get(0);
-        assertTrue(actual.contains("/api/create"));
-    }    
-   /*
-    @Test
-    public void testRetrieveStudent() throws Exception {
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-        ResponseEntity<String> response = restTemplate.exchange(
-          createURLWithPort("/api/fetch?shortCode=" + shortCode), HttpMethod.GET, entity, String.class);
-        String expected = "{\"id\":1,\"name\":\"Rajesh Bhojwani\",\"description\":\"Class 10\"}";
-        JSONAssert.assertEquals(expected, response.getBody(), false);
-    }
-    */
-    
-    /*
-    @Test
-    public void saveURL() throws Exception {
-        assertThat(
-                this
-                .restTemplate
-                .getForObject("http://localhost:" + port + "/api/create?longUrl=" +longURL,                
-                String.class)).contains("Hello World");
-    }
-    */
-    /*
-    @Test
-    public void getURL() throws Exception {
-        assertThat(
-                this
-                .restTemplate
-                .getForObject("http://localhost:" + port + "/api/fetch?shortCode=" +shortCode,                
-                String.class)).isNotEmpty();
-    }
-    */
-    //@Autowired
-    //private MockMvc mockMvc;
-    
-   // String longURL = "http://abcd.com/test";
-        
-    //String shortCode = "abcdfer";
-    //@MockBean
-       /*                
-    @Test
-    void longToShort() throws Exception {
-        this.mockMvc
-            .perform(post("/api/create?longUrl=" + longURL)
-            .contentType(MediaType.APPLICATION_JSON)                    
-            .accept(MediaType.APPLICATION_JSON))
-                    //.andDo(print())
-            .andExpect(status().isCreated());
-                    //.andExpect((ResultMatcher) mock);
-                    //.andExpect(content().string(containsString(shortCode))); 
-    }
-        
-    @Test
-    void shortToLong() throws Exception {
-        this.mockMvc
-            .perform(get("/api/fetch?shortCode=" + shortCode)
-            .contentType(MediaType.APPLICATION_JSON)                    
-            .accept(MediaType.APPLICATION_JSON))
-                    //.andDo(print())
-            .andExpect(status().isOk());
-                    //.andExpect((ResultMatcher) mock);                    
-                    //.andExpect(content().string(containsString(longURL)));
-        } 
-}
-*/
 
--- shortener/src/test/java/com/urlshortener/shortener/ShortenerApplicationTest.java
+++ shortener/src/test/java/com/urlshortener/shortener/ShortenerApplicationTest.java
@@ -1,6 +1,6 @@
 package com.urlshortener.shortener;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import static org.hamcrest.CoreMatchers.containsString;
 import org.junit.runner.RunWith;
 import static org.mockito.Mockito.when;
@@ -18,16 +18,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ApiController.class) 
-class ShortenerApplicationTest {
-    // test cases for controller and service
-    
+public class ShortenerApplicationTest {
         @Autowired
         private MockMvc mockMvc;
         
@@ -35,52 +32,30 @@ class ShortenerApplicationTest {
         private ShortUrlService service;
         
         String longURL = "http://abcd.com/test";
-        
         String shortCode = "abcdfer";
-        //@MockBean
         final ShortCode mock = new ShortCode(shortCode);
-        
-        //@MockBean
         final LongUrl mock2 = new LongUrl(longURL);
-        
               
 	@Test
-	void longToShort() throws Exception {          
+	public void longToShort() throws Exception {          
             when(service.saveUrl(longURL)).thenReturn(mock);
                        
             this.mockMvc
                     .perform(post("/api/create?longUrl=" + longURL)
                     .contentType(MediaType.APPLICATION_JSON)                    
                     .accept(MediaType.APPLICATION_JSON))
-                    //.andDo(print())
                     .andExpect(status().isCreated())
-                    //.andExpect((ResultMatcher) mock);
                     .andExpect(content().string(containsString(shortCode)));
-            // */
-            
-             /* when run this code then test works (without Mockito.verify)
-            Mockito.when(service.saveUrl(longUrl)).thenReturn(mock);
-            
-            RequestBuilder rb = MockMvcRequestBuilders.post("/api/create").accept(MediaType.APPLICATION_JSON);
-            
-            MvcResult mvcResult = mockMvc.perform(rb).andReturn();
-            
-            System.out.println(mvcResult.getResponse());
-            
-            //Mockito.verify(service).saveUrl(longUrl); 
-             */
 	}
         
         @Test
-        void shortToLong() throws Exception {
+        public void shortToLong() throws Exception {
             when(service.getUrl(shortCode)).thenReturn(mock2);
             this.mockMvc
                     .perform(get("/api/fetch?shortCode=" + shortCode)
                     .contentType(MediaType.APPLICATION_JSON)                    
                     .accept(MediaType.APPLICATION_JSON))
-                    //.andDo(print())
                     .andExpect(status().isOk())
-                    //.andExpect((ResultMatcher) mock);
                     .andExpect(content().string(containsString(longURL)));
         } 
 
