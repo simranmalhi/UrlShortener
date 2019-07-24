89d1792a09a90bd8c6e14cd931317e4bc53c62c7
simranmalhi <46908296+simranmalhi@users.noreply.github.com>
2019-07-24 14:36:17 -0700

    Commit 2

--- shortener/src/main/java/com/urlshortener/shortener/ShortenerApplication.java
+++ shortener/src/main/java/com/urlshortener/shortener/ShortenerApplication.java
@@ -2,7 +2,7 @@ package com.urlshortener.shortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
 //@EnableJpaAuditing
